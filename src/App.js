13e8547fe33993064e83946039497fa3985a8b43
Ahmed-Gamil-osman <ahmed.gamil@student.aast.edu>
2026-03-13 22:38:52 +0200

    changed the website

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Now we are using bind mount volumes for both Dev and Test
+          We can now Deploy our React app Gamed ya Gamed
         </p>
         <a
           className="App-link"
